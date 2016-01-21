f818a25c08d8837925ee0f29f24432b7c1302622
andrey <andrey@cossacklabs.com>
2016-01-21 16:29:17 +0200

    Add some corrections

--- server.py
+++ server.py
@@ -40,7 +40,7 @@ async def wshandler(request):
             #if msg.is_control:
             #    ws.send_str(base64.b64encode(msg));
             #else:
-                msg = mwsg.data
+                msg = mesg.data
                 print('request:'+msg)
                 arr = msg.split()
                 if arr[0] == "NEWROOM" and pub_key != "":
@@ -49,13 +49,13 @@ async def wshandler(request):
                     ws.send_str('NEWROOM '+new_room_id)
                 elif arr[0] == "INVITE" and pub_key != "":
                     if arr[2] in rooms and arr[3] == rooms[arr[2]]['owner'] and arr[3] in online:
-                        online[arr[3]].send_str(msg.data)
+                        online[arr[3]].send_str(msg)
                     else:
                         ws.send_str('INVALIDINVITE '+arr[1])
                 elif arr[0] == "INVITE_RESP" and pub_key != "":
                     if arr[1] in online and pub_key == rooms[arr[2]]['owner']:
                         rooms[arr[2]]['users'].append(arr[1])
-                        online[arr[1]].send_str(msg.data)
+                        online[arr[1]].send_str(msg)
                 elif arr[0] == "OPENROOM" and pub_key != "":
                     if arr[1] in rooms and pub_key in rooms[arr[1]]['users']:
                         ws.send_str('OPENROOM 111')
@@ -69,23 +69,29 @@ async def wshandler(request):
                             else:
                                 if ww in history:
                                     history[ww].append(msg)
-                                else
+                                else:
                                     history[ww]=[msg]
                     else:
                         ws.send_str('ERRORMESSAGE '+arr[1])
                 elif arr[0] == 'PUBKEY':
                     pub_key=arr[1]
                     online[pub_key]=ws
-                    for mm in history[pub_key]:
-                        ws.send_str(mm)
-                    history[pub_key]=[]
+                    if pub_key in history:
+                        for mm in history[pub_key]:
+                           ws.send_str(mm)
+                        history[pub_key]=[]
                 else:
                     ws.send_str('ERROR malformed request');
-        elif msg.tp == web.MsgType.close:
+        elif mesg.tp == aiohttp.MsgType.error:
+             online.remove(pub_key);
+             print("closed");
+        elif mesg.tp == web.MsgType.close:
             print('connection closed')
         else:
             ws.send_str('ERROR malformed request')
-            print('error:'+msg.data)
+            print('error:'+msg)
+    del online[pub_key];
+    print("closed");
     return ws
 
 async def init(loop):
@@ -93,7 +99,7 @@ async def init(loop):
     app.router.add_route('GET', '/', handle)
     app.router.add_route('GET', '/ws', wshandler)
 
-    srv = await loop.create_server(app.make_handler(), '0.0.0.0', 8888)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8888)
     print("Server started at http://127.0.0.1:8888")
     return srv
 
